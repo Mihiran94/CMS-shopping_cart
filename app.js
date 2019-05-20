a709a4abb81e68455d2afead7f218180ca4cfd61
Mihiran94 <mihiran.ponnamperuma@gmail.com>
2019-05-20 13:12:31 +0530

    database connected

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+
+//connect to db
+
+mongoose.connect(config.database);
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error!'));
+db.once('open',function(){
+    console.log('connected to mongo db');
+});
+
 
 //init app
 const app = express();
